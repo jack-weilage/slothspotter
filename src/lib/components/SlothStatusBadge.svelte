a0c2e275722e2a35baf6e9d04468c1b259b283cb
Jack Weilage <jack@weilage.dev>
2025-08-28 15:34:38 -0700

    feat: Better badge for status

---
+++ src/lib/components/SlothStatusBadge.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { SlothStatus } from "$lib/client/db/schema";
+	import { Badge } from "$lib/components/ui/badge";
+
+	let { status }: { status: SlothStatus } = $props();
+</script>
+
+<Badge variant={status === SlothStatus.Active ? "active" : "inactive"}>
+	{status === SlothStatus.Active ? "Active" : "Removed"}
+</Badge>
