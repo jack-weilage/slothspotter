6e63c861b5f1b7d5221de9476cd1ef64f4fb6682
Jack Weilage <jack@weilage.dev>
2025-08-28 20:44:22 -0700

    feat: Create a new brandable logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+	import faviconDark from "$lib/assets/favicon-dark.svg";
+	import faviconLight from "$lib/assets/favicon-light.svg";
 	import "../app.css";
-
-	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={faviconLight} media="(prefers-color-scheme: light)" />
+	<link rel="icon" href={faviconDark} media="(prefers-color-scheme: dark)" />
 </svelte:head>
 
 {@render children()}
